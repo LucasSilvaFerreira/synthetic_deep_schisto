6c17bef8c435bb742633708c6f903921f38fb9a8
lucas <bioinfolucas@gmail.com>
2016-08-24 18:44:42 -0300

    -Adding distance factor to separate parasites

--- scripts/processing.py
+++ scripts/processing.py
@@ -6,10 +6,18 @@ from scipy.ndimage import imread
 import Image
 import random
 from tqdm import tqdm
+import numpy as np
+
+def check_distance(value, distance_factor=50):
+
+    if sum(value) > distance_factor:
+        return True
+    else:
+        return False
 
 def main():
     VAL_RATIO = 0.2 #max 1
-    N_PICS = 5000
+    N_PICS = 100
     DENSITY_VALUE = 3
     bg = Image.open("/home/lucas/PycharmProjects/synthetic_deep_schisto/cells_models/bg.png")
     print bg.size
@@ -32,17 +40,35 @@ def main():
 
 
         density_generator = random.randint(1, DENSITY_VALUE)
+        density_generator_history = []
         bg_frame = bg.copy()
         density_in_field = 0
         for x in range(density_generator):
+
+            if len(density_generator_history) != 0 :
+                result =  density_generator_history[0]
+                #print list(set([ check_distance(map(abs, np.array(x_random) - result)) for x_random in density_generator_history]))
+                while False in list(set([check_distance(map(abs, np.array(x_random) - result), distance_factor=50) for x_random in density_generator_history]))  :
+                    print  np.array(x_random), result, 'teste'
+                    x_coor = random.randint(-10, 140)
+                    y_coor = random.randint(-10, 140)
+                    result = np.array([x_coor, y_coor])
+                    print result
+                    #print set([check_distance(map(abs, np.array(x_random) - result)) for x_random in density_generator_history])
+
+
+            else:
+
+                x_coor = random.randint(-10, 140)
+                y_coor = random.randint(-10, 140)
+                np.array([x_coor, y_coor])
 
             schisto_r = schisto.copy() # essa parte devera captuar um schisto sample aleatorio
             schisto_r = schisto_r.rotate(random.randint(0, 360))
 
             w_to_subtract = white.copy()
 
-            x_coor = random.randint(-10, 140)
-            y_coor = random.randint(-10, 140)
+            density_generator_history.append(np.array([x_coor,y_coor ]))
 
             w_to_subtract.paste(schisto_r, (x_coor, y_coor), schisto_r)
 
